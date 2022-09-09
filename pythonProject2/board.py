81bd59f23d006f03f4811ce3346b266084da7bed
clever-little-boy <3392592354@qq.com>
2022-09-09 20:24:12 +0800

    测试提交

---
+++ pythonProject2/board.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+'''棋盘模块
+棋盘每一行三个元素可有o、x、  、三类填入。
+'''
+#显示棋盘提示信息。
+#初始化模块
+class Board(object):  #定义类
+    def __init__(self): #初始化数据固定不需自己传
+        self.board_data = [' ']*9 #记录每个位置棋子  [' ']*9  -->[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        self.moveble_list = list(range(9))  #剩余可放棋子的位子 。。生成可移动列表、、list(range(9)) ---> [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    def show_board(self):
+        '''显示棋盘'''
+        for i in (0,3,6):
+            print('       |       |       ')
+            print('   %d   |   %d   |   %d   '%(i,i+1,i+2))
+            print('       |       |       ')
+            if i!=6:
+                print('-'*23)
+
+if __name__ == '__main__': #测试模块初始化
+    #棋盘测试
+    board=Board()#创建棋盘对象
+    #print(board.board_data)
+    #print(board.moveble_list)
+    #测试打印棋盘
+    board.show_board()
