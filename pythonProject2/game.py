e0c4700ad45368272d215fa275192043285c3f9a
clever-little-boy <3392592354@qq.com>
2022-09-18 23:37:47 +0800

    一个游戏的测试

--- pythonProject2/game.py
+++ pythonProject2/game.py
@@ -24,19 +24,42 @@ class Game(object):
     def play_around(self):
         '''一局游戏'''
         #显示棋盘
+        self.chess_board.show_board(True)
         #决定先手玩家
+        current_player,next_player = self.radom_player()
+
         while True:
             #下子方落子
+            current_player.move(self.chess_board)
             #显示棋盘
+            self.chess_board.show_board()
             #判断是否胜利
+            if self.chess_board.is_win(current_player.chess):
+                current_player.score +=1
+                print('%s 战胜了 %s' %(current_player,next_player))
+                break
             #是否和局
+            if self.chess_board.is_dogfall():
+                break
             #交换落子坊
-            pass
-    #显示比分
+            current_player,next_player = next_player,current_player
+        #显示比分
+        print('[%s]对战[%s]比分是：%d:%d' %(current_player,
+                                          next_player,
+                                          current_player.score,
+                                          next_player.score))
+    def start(self):
+        '''开启有戏，能够循环对局'''
+        #开始游戏
+        #让用户选择是否再来一局
+        #判断是否退出游戏
+        #重置棋盘数据
 if __name__ == '__main__':
     #测试游戏初始化
     game = Game()
     # print(game.chess_board.moveble_list)
     # print(game.human.name)
     # print(game.computer.name)
-    print(game.radom_player()[0].name)
+    # print(game.radom_player()[0].name)
+    #测试游戏
+    game.play_around()
