64f6bb3b3ae0ce5c4eb774a69a6e83ccab789dd9
clever-little-boy <3392592354@qq.com>
2022-09-18 13:47:48 +0800

    注释

--- pythonProject2/game.py
+++ pythonProject2/game.py
@@ -1,2 +1,42 @@
 # coding:utf-8
 '''游戏魔块'''
+import board
+import player
+import random
+class Game(object):
+    def __init__(self):
+        '''初始化游戏数据'''
+        self.chess_board = board.Board()
+        self.human = player.Player('玩家')
+        self.computer = player.Player('电脑')
+    def radom_player(self):
+        '''随机确定玩家先后手顺序'''
+        if random.randint(0,1) == 1: #随机生成一个0或者1的整数
+            player = (self.human,self.computer)
+        else:
+            player = (self.computer,self.human)
+
+        player[0].chess = 'X'
+        player[1].chess = 'O'
+
+        print('根据随机结果 %s 先行'% player[0].name)
+        return player
+    def play_around(self):
+        '''一局游戏'''
+        #显示棋盘
+        #决定先手玩家
+        while True:
+            #下子方落子
+            #显示棋盘
+            #判断是否胜利
+            #是否和局
+            #交换落子坊
+            pass
+    #显示比分
+if __name__ == '__main__':
+    #测试游戏初始化
+    game = Game()
+    # print(game.chess_board.moveble_list)
+    # print(game.human.name)
+    # print(game.computer.name)
+    print(game.radom_player()[0].name)
--- pythonProject2/player.py
+++ pythonProject2/player.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-'''游戏主模块'''
+'''玩家主模块'''
 import board
 class Player(object):
     def __init__(self,name):
@@ -11,14 +11,14 @@ class Player(object):
         '''玩家落一个子到棋盘上'''
         index = int(input("请 %s 输入落子位置 %s :" % (self.name, board.moveble_list)))
         board.move_down(index,self.chess)
-if __name__ == '__main__':
-    player1 = Player("玩家1")
-    player1.chess = 'X'
+# if __name__ == '__main__':
+#     player1 = Player("玩家1")
+#     player1.chess = 'X'
     # print(player1.name)
     # print(player1.score)
     # print(player1.chess)
 
     #测试玩家落子功能
-    board = board.Board()
-    player1.move(board)
-    board.show_board()
+    # board = board.Board()
+    # player1.move(board)
+    # board.show_board()
